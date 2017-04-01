f2f87743f65da34dd26bcbb68678a4a0920f2ced
jestersimpps <jestersimpps@gmail.com>
2017-04-01 13:20:02 +0200

    sentiment config

--- colorTroll.js
+++ colorTroll.js
@@ -1,6 +1,50 @@
 var autobahn = require('autobahn');
 var colors = require('colors');
 var request = require('request');
+fs = require('fs');
+
+
+
+var bull = fs.readFileSync(`./sentimentconfig/bull.txt`, 'utf8').split(/\r?\n/);;
+var bear = fs.readFileSync(`./sentimentconfig/bear.txt`, 'utf8').split(/\r?\n/);
+var neutral = fs.readFileSync(`./sentimentconfig/neutral.txt`, 'utf8').split(/\r?\n/);
+
+
+function colorStrings(words) {
+    var wordsArray = words.split(/[\s,.\/?!]+/);
+    var sentence = '';
+    wordsArray.forEach(word => {
+        if (neutral.includes(word.toLowerCase())) {
+            sentence += word.blue;
+        }
+        if (bull.includes(word.toLowerCase())) {
+            sentence += word.green;
+            sentiment = 1;
+        }
+        else if (bear.includes(word.toLowerCase())) {
+            sentence += word.red;
+        }
+        else if (currencies.includes(word.toLowerCase())) {
+            sentence += word.bold.yellow.underline;
+        } else {
+            sentence += word;
+        }
+        sentence += ' ';
+    })
+    return sentence;
+}
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body);
+        for (var x in info) {
+            currencies.push(x.toLowerCase());
+            currencies.push(info[x].name.toLowerCase());
+        }
+    }
+}
+
+
 
 var wsuri = "wss://api.poloniex.com";
 var connection = new autobahn.Connection({
@@ -14,42 +58,7 @@ var options = {
     url: 'https://poloniex.com/public?command=returnCurrencies',
 };
 
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body);
-        for (var x in info) {
-            currencies.push(x.toLowerCase());
-            currencies.push(info[x].name.toLowerCase());
-        }
-    }
-}
-
 request(options, callback);
-
-
-function colorStrings(words) {
-    var wordsArray = words.split(/[\s,.\/?!]+/);
-    var sentence = '';
-    wordsArray.forEach(word => {
-        if (['whale','whales','action','wall'].includes(word.toLowerCase())) {
-            sentence += word.blue;
-        }
-        if (['pump', 'up', 'moon', 'sky', 'long', 'hodl', 'hold', 'buy','jumping','undervalued','mooning'].includes(word.toLowerCase())) {
-            sentence += word.green;
-            sentiment = 1;
-        }
-        else if (['dump', 'down', 'crashing', 'falling', 'sell','crash'].includes(word.toLowerCase())) {
-            sentence += word.red;
-        }
-        else if (currencies.includes(word.toLowerCase())) {
-            sentence += word.bold.yellow.underline;
-        } else {
-            sentence += word;
-        }
-        sentence += ' ';
-    })
-    return sentence;
-}
 
 connection.onopen = function (session) {
     function trollboxEvent(args, kwargs) {
