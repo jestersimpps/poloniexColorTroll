b2e221f2a8dea34f5c0894579efbe34cd7041dc9
jestersimpps <jestersimpps@gmail.com>
2017-04-01 16:14:42 +0200

    sentiment

--- colorTroll.js
+++ colorTroll.js
@@ -1,36 +1,51 @@
 var autobahn = require('autobahn');
 var colors = require('colors');
 var request = require('request');
+var sentiment = require('sentiment');
 
 fs = require('fs');
-
 
 
 var bull = fs.readFileSync(`./sentimentconfig/bull.txt`, 'utf8').split(/\r?\n/);;
 var bear = fs.readFileSync(`./sentimentconfig/bear.txt`, 'utf8').split(/\r?\n/);
 var neutral = fs.readFileSync(`./sentimentconfig/neutral.txt`, 'utf8').split(/\r?\n/);
 
+onlyUnique = (value, index, self) => {
+    return self.indexOf(value) === index;
+}
 
 colorStrings = (words) => {
+    var sentimentScore = sentiment(words.toString()).score;
     var wordsArray = words.split(/[\s,.\/?!]+/);
     var sentence = '';
+    var sentenceCurrencies = [];
     wordsArray.forEach(word => {
         if (neutral.includes(word.toLowerCase())) {
             sentence += word.blue;
         }
         else if (bull.includes(word.toLowerCase())) {
             sentence += word.green;
+            sentimentScore += 2;
         }
         else if (bear.includes(word.toLowerCase())) {
             sentence += word.red;
+            sentimentScore += -2;
         }
         else if (currencies.includes(word.toLowerCase())) {
             sentence += word.bold.yellow.underline;
+            sentenceCurrencies.push(word.toLowerCase());
         } else {
             sentence += word;
         }
         sentence += ' ';
     })
+    if (sentenceCurrencies.length) {
+        if (sentimentScore < 0) {
+            sentence += ' ('.red + sentenceCurrencies.filter(onlyUnique).join(' ').red + sentimentScore.toString().red + ' )'.red;
+        } else if (sentimentScore > 0) {
+            sentence += ' ('.green + sentenceCurrencies.filter(onlyUnique).join(' ').green + ' +'.green + sentimentScore.toString().green + ' )'.green;
+        } 
+    }
     return sentence;
 }
 
