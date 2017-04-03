8ecae816d26b5a271a25a5128b268ec2f10c8a31
jestersimpps <jestersimpps@gmail.com>
2017-04-04 00:12:17 +0200

    async

--- colorTroll.js
+++ colorTroll.js
@@ -2,17 +2,43 @@ var autobahn = require('autobahn');
 var colors = require('colors');
 var request = require('request');
 var sentiment = require('sentiment');
-var db = require('diskdb');
 var fs = require('fs');
+var url = 'mongodb://localhost:27017/polo';
+var mongoose = require('mongoose');
 
-db.connect('./data', ['currencies']);
+mongoose.connect(url);
+var db = mongoose.connection;
 
+db.on('error', function (err) {
+    console.log('connection error', err);
+});
+db.once('open', function () {
+    console.log('mongod connected.');
+});
 
-var currencies = db.currencies.find();
+var Currency = mongoose.model('currencies', {
+    shortHand: String,
+    lowerCase: String,
+    fullName: String,
+    sentiment: Number,
+    btclast: Number,
+    usdlast: Number,
+    ethlast: Number
+});
+
+var Trade = mongoose.model('trades', {
+    action: String,
+    shortHand: String,
+    usdlast: Number
+});
 
 var bull = fs.readFileSync(`./sentimentconfig/bull.txt`, 'utf8').split(/\r?\n/);;
 var bear = fs.readFileSync(`./sentimentconfig/bear.txt`, 'utf8').split(/\r?\n/);
 var neutral = fs.readFileSync(`./sentimentconfig/neutral.txt`, 'utf8').split(/\r?\n/);
+
+var CurrentlyBought = mongoose.model('currentlyBought', {
+    shortHand: String
+});
 
 onlyUnique = (value, index, self) => {
     return self.indexOf(value) === index;
@@ -23,64 +49,73 @@ colorStrings = (words) => {
     var wordsArray = words.split(/[\s,.\/?!+-]+/);
     var sentence = '';
     var sentenceCurrencies = [];
-    wordsArray.forEach(word => {
-        if (neutral.includes(word.toLowerCase())) {
-            sentence += word.blue;
-        }
-        else if (bull.includes(word.toLowerCase())) {
-            sentence += word.green;
-            sentimentScore += 3;
-        }
-        else if (bear.includes(word.toLowerCase())) {
-            sentence += word.red;
-            sentimentScore -= 3;
-        }
-        else if (getCurrencyNamesArray().includes(word.toLowerCase())) {
-            sentence += word.bold.yellow.underline;
-            var currencyObject = getCurrencyObject(word.toLowerCase());
-            decreaseSentiment(currencyObject.shortHand);
-            sentenceCurrencies.push(currencyObject.shortHand);
-        } else {
-            sentence += word;
-        }
-        sentence += ' ';
+    return getCurrencyNamesArray().then(currencyNamesArray => {
+        wordsArray.forEach(word => {
+            getCurrencyObject(word.toLowerCase()).then(currencyObject => {
+                if (neutral.includes(word.toLowerCase())) {
+                    sentence += word.blue;
+                }
+                else if (bull.includes(word.toLowerCase())) {
+                    sentence += word.green;
+                    sentimentScore += 3;
+                }
+                else if (bear.includes(word.toLowerCase())) {
+                    sentence += word.red;
+                    sentimentScore -= 3;
+                }
+                else if (currencyNamesArray.includes(word.toLowerCase())) {
+                    sentence += word.bold.yellow.underline;
+                    decreaseSentiment(currencyObject.shortHand);
+                    sentenceCurrencies.push(currencyObject.shortHand);
+                } else {
+                    sentence += word;
+                }
+                sentence += ' ';
+                sentenceCurrencies.forEach(sc => {
+                    sentence += generateSentiment(sc.toLowerCase(), sentimentScore);
+                });
+                return sentence;
+            });
+        });
+
     })
-    sentenceCurrencies.forEach(sc => {
-        sentence += generateSentiment(sc.toLowerCase(), sentimentScore);
-    })
-    return sentence;
+
+
 }
 
 
 generateSentiment = (currency, sentimentScore) => {
-    var currentSentiment = getCurrentSentiment(currency);
-    var newSentiment = currentSentiment + sentimentScore;
-    var sentimentPart = ' (' + currency.bold
-        + ': '
-        + colorSentiment(newSentiment) + ') ';
-    if (newSentiment < 20 && newSentiment > -20) {
-        storeSentiment(currency, newSentiment);
-    }
-    if (newSentiment < 0) {
-        return colors.italic.red(sentimentPart);
-    } else if (newSentiment > 0) {
-        return colors.italic.green(sentimentPart);
-    } else {
-        return colors.italic.blue(sentimentPart);
-    }
+    getCurrentSentiment(currency).then(currentSentiment => {
+        var newSentiment = currentSentiment + sentimentScore;
+        var sentimentPart = ' (' + currency.bold
+            + ': '
+            + colorSentiment(newSentiment) + ') ';
+        if (newSentiment < 20 && newSentiment > -20) {
+            storeSentiment(currency, newSentiment);
+        }
+        if (newSentiment < 0) {
+            return colors.italic.red(sentimentPart);
+        } else if (newSentiment > 0) {
+            return colors.italic.green(sentimentPart);
+        } else {
+            return colors.italic.blue(sentimentPart);
+        }
+    });
 }
 
 
 decreaseSentiment = (upCurrencyShortHand) => {
-    db.currencies.find().forEach(c => {
-        if (c.shortHand != upCurrencyShortHand) {
-            if (c.sentiment < 0) {
-                db.currencies.update({ fullName: c.fullName }, { sentiment: c.sentiment + 1 });
-            } else if (c.sentiment > 0) {
-                db.currencies.update({ fullName: c.fullName }, { sentiment: c.sentiment - 1 });
+    Currency.find({}, currencies => {
+        currencies.forEach(c => {
+            if (c.shortHand != upCurrencyShortHand) {
+                if (c.sentiment < 0) {
+                    Currency.update({ fullName: c.fullName }, { sentiment: c.sentiment + 1 });
+                } else if (c.sentiment > 0) {
+                    Currency.update({ fullName: c.fullName }, { sentiment: c.sentiment - 1 });
+                }
             }
-        }
-    })
+        })
+    });
 }
 
 colorSentiment = (sentiment) => {
@@ -94,30 +129,44 @@ colorSentiment = (sentiment) => {
 }
 
 storeSentiment = (currency, newSentiment) => {
-    db.currencies.update({ lowerCase: currency }, { sentiment: newSentiment });
-    db.currencies.update({ fullName: currency }, { sentiment: newSentiment });
+    Currency.update({ lowerCase: currency }, { sentiment: newSentiment });
+    Currency.update({ fullName: currency }, { sentiment: newSentiment });
 }
 
 getCurrencyObject = (currency) => {
-    var currencyObject = db.currencies.findOne({ lowerCase: currency });
-    if (!currencyObject) { currencyObject = db.currencies.findOne({ fullName: currency }); }
-    return currencyObject;
+    return Currency.findOne({ lowerCase: currency }, currencyObject1 => {
+        if (!currencyObject1) {
+            return Currency.findOne({ fullName: currency }, currencyObject2 => {
+                return currencyObject2;
+            });
+        } else {
+            return {};
+        }
+    });
 }
 
 getCurrentSentiment = (c) => {
-    var currency = db.currencies.findOne({ lowerCase: c });
-    if (!currency) { currency = db.currencies.findOne({ fullName: c }) };
-    return currency ? currency.sentiment : 0;
+    return Currency.findOne({ lowerCase: c }, currency1 => {
+        if (!currency1) {
+            return Currency.findOne({ fullName: c }, currency2 => {
+                return currency2 ? currency2.sentiment : 0;
+            })
+        } else {
+            return 0
+        };
+    });
 }
 
 getCurrencyNamesArray = () => {
-    var names = [];
-    db.currencies.find().forEach(c => {
-        names.push(c.lowerCase);
-        names.push(c.fullName);
-        names.push(c.fullName + 's');
+    return Currency.find({}, currencies => {
+        var names = [];
+        currencies.forEach(c => {
+            names.push(c.lowerCase);
+            names.push(c.fullName);
+            names.push(c.fullName + 's');
+        })
+        return names;
     })
-    return names;
 }
 
 normalizeName = (name) => {
@@ -133,11 +182,13 @@ var connection = new autobahn.Connection({
 
 connection.onopen = (session) => {
     trollboxEvent = (args, kwargs) => {
-        var troll = {
-            name: normalizeName(args[2]),
-            message: colorStrings(args[3])
-        };
-        console.log(troll.name + troll.message);
+        colorStrings(args[3]).then(colorSentence => {
+            var troll = {
+                name: normalizeName(args[2]),
+                message: colorSentence
+            };
+            console.log(troll.name + troll.message);
+        })
     }
     session.subscribe('trollbox', trollboxEvent);
 }
